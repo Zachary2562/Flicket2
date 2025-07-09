8c6af2cee85b4ab1b97ba808adcf9ad3f1a3339e
Zachary2562 <zachary.kofman2@gmail.com>
2025-07-08 23:18:19 -0400

    Update run.py

--- run.py
+++ run.py
@@ -2,3 +2,8 @@ from application import app
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8000)
+from flask import send_file
+
+@app.route('/download-db')
+def download_db():
+    return send_file('flicket.db', as_attachment=True)
